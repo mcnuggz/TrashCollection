e8d3eb3ca935c6ec97b68649ede086386606bdf3
Ryan Webb <dzanfox@gmail.com>
2016-02-23 12:03:42 -0600

    restarted project

--- TrashCollection/TrashCollection/Properties/AssemblyInfo.cs
+++ TrashCollection/TrashCollection/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f27bf4b6-a6c5-4cdd-bf70-effdee6be2c4")]
+[assembly: Guid("0e3edf88-47f6-49dd-9b4c-5cb3fe48ecfc")]
 
 // Version information for an assembly consists of the following four values:
 //
